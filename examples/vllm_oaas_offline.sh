ea567781606b54f23a7abd5becfe0a51f8ac5b4f
Chen Wu <chenw@sambanovasystems.com>
2025-06-13 22:07:20 +0000

    Update vllm_oaas_offline.sh to set MODEL_PATH for Llama-2-7b-chat-hf-bf16 and adjust model path argument for llm_analyzer_vllm_oaas_async_ra.py execution.

--- examples/vllm_oaas_offline.sh
+++ examples/vllm_oaas_offline.sh
@@ -2,8 +2,12 @@
 # CUDA_VISIBLE_DEVICES=3 python3 vllm_oaas_async.py
 # CUDA_VISIBLE_DEVICES=3 python3 llm_analyzer_vllm_oaas.py
 # CUDA_VISIBLE_DEVICES=3 python3 llm_analyzer_vllm_oaas_async.py \
-CUDA_VISIBLE_DEVICES=3 python3 llm_analyzer_vllm_oaas_async_ra.py \
+
+# MODEL_PATH=/nvmedata/hf_checkpoints/Qwen3-8B/
+MODEL_PATH=/nvmedata/hf_checkpoints/Llama-2-7b-chat-hf-bf16
+
+CUDA_VISIBLE_DEVICES=2 python3 llm_analyzer_vllm_oaas_async_ra.py \
     --input_path /nvmedata/chenw/genz/genz_users_20k_format.tsv \
     --output_path /nvmedata/chenw/genz/genz_users_interests_vllm_oaas_async.jsonl \
-    --model_path /nvmedata/hf_checkpoints/Qwen3-8B/ \
+    --model_path $MODEL_PATH \
     --batch_size 32
